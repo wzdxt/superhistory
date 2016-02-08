ce7a963deda5f697f99e799f0e22c848e891ffe2
wzdxt <wzdxt0624@gmail.com>
2016-02-08 16:56:23 +0800

    update
    Signed-off-by: wzdxt <wzdxt0624@gmail.com>

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -4,7 +4,7 @@ class Visit < ActiveRecord::Base
   scope :for_user, -> (user) { where :user => user }
   scope :for_url, -> (url) { where :url => url }
   scope :not_closed, -> { where :close_time => nil }
-  scope :contains_localhost, -> {where visit[:url].matches('://localhost/%').or visit[:url].matches('://localhost:%')}
+  scope :contains_localhost, -> {where visit[:url].matches('%://localhost/%').or visit[:url].matches('%://localhost:%')}
 
   def self.filter_existed_local
     self.contains_localhost.delete_all
